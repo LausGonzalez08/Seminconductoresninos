d89836402671e47b7657a8606bfd76efeff2be46
Kaouru Tanamachi <lg489119@gmail.com>
2025-10-31 02:01:04 -0600

    Rediseño y edad
    Rediseño y edad

--- app/src/main/java/com/example/seminconductoresninos/Home.java
+++ app/src/main/java/com/example/seminconductoresninos/Home.java
@@ -1,19 +1,31 @@
 package com.example.seminconductoresninos;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class Home extends AppCompatActivity {
+    Spinner Edad;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_home);
+
+        // 🔽 Aquí sí puedes vincular la vista
+        Edad = findViewById(R.id.spinnerEdad);
+
+        // Configurar el adaptador del spinner
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_spinner_item,
+                new String[]{"Seleccionar", "10", "11", "12", "13", "14", "15", "16", "17", "18"}
+        );
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        Edad.setAdapter(adapter);
     }
 }
